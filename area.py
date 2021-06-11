62f13f5b485df7ad346547e2a5459cda17a2691c
Turodoras <turodoras@gmail.com>
2021-06-11 15:20:30 +0200

    Update given files and add viewer for json file

--- area.py
+++ area.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from enum import IntEnum
 from shapely.geometry import mapping, Polygon, Point, LineString, MultiPolygon
 from shapely import ops
@@ -72,6 +70,10 @@ class Area():
     def identity(self):
         return self._id
 
+    @property
+    def polygon(self):
+        return self._polygon
+
     def split(self, percentage, direction, inplace=True, new_category=Category.GARDEN):
         """
         Split an area in two areas. Store result in self.sub_areas if inplace == True.
@@ -87,24 +89,29 @@ class Area():
 
         Tests:
             >>> surf = Area(Polygon([(0,0), (20,0), (20,40), (0,40)]), Category.HOUSE)
-            >>> surf.split(0.5, 0, False)  # house takes 1/2 of surface and is north
-            (Category.HOUSE:POLYGON ((20 20, 20 0, 0 0, 0 20, 20 20)),
-            Category.GARDEN:POLYGON ((0 20, 0 40, 20 40, 20 20, 0 20)))
+            >>> res = surf.split(0.25, 0, False)  # house takes 1/4 of surface and is north
+            >>> res0 = Polygon([(0, 30), (0, 40), (20, 40), (20, 30), (0, 30)])
+            >>> res0.symmetric_difference(res[0].polygon).area < 1
+            True
         """
+        assert(percentage > 0)
+        if not self._polygon.exterior.is_ccw: # should be counter clockwise
+            coords = list(self._polygon.exterior.coords)
+            self._polygon = Polygon(coords[::-1])
         direction = np.deg2rad(90 - direction)  # degrees are cw while radian are ccw + 0 is North
         pts = np.array(self._polygon.minimum_rotated_rectangle.exterior.coords)
         diameter = np.sqrt(np.sum(np.square(pts[2] - pts[0])))
         start = np.array(self._polygon.centroid)
         end = start +  np.array([diameter * np.cos(direction), diameter * np.sin(direction)])
         path = LineString([start, end])
-        res = path.intersection(self._polygon.boundary)
+        pt_intersection = path.intersection(self._polygon.boundary)
         try:
-            res = list(res)[-1]  # we may have more than one intersection
+            pt_intersection = list(pt_intersection)[-1]  # we may have more than one intersection
         except:
             pass
         pts = self._polygon.boundary.coords
         for pt1, pt2 in zip(pts[:-1], pts[1:]):
-            if LineString((pt1, pt2)).distance(res) < 1E-6:
+            if LineString((pt1, pt2)).distance(pt_intersection) < 1E-6:
                 break
         pt1 = np.array(pt1)
         pt2 = np.array(pt2)
@@ -112,19 +119,24 @@ class Area():
         dir = (pt2 - pt1) / dist
         orth = np.array([-dir[1], dir[0]])  # ccw
         house_area = self._polygon.area * percentage
-        width = house_area / dist  # ok only if we have a rectangle
+        width = diameter / 2 # hence we can reach from 0 to diameter
         res = [self._polygon,]
-        while abs(res[0].area - house_area) > 1:  # 1 meterÂ² error accepted
-            # print(".", end="")
+        dw = width
+        while abs(res[0].area - house_area) > 1:  # 1 meter² error accepted
             cut = LineString([pt1 + width * orth - diameter * dir, pt2 + width * orth + diameter * dir])
             res = ops.split(self._polygon, cut)
-            res = MultiPolygon([g for g in res])
-            if res[0].area > house_area:
-                width /= 2
+            dw /= 2
+            if len(res) == 0:
+                width -= dw
+                continue
+            if pt_intersection.distance(res[0]) > pt_intersection.distance(res[1]):
+                res = MultiPolygon([res[1], res[0]])
             else:
-                width *= 1.5
-            if width > diameter:
-                width = diameter
+                res = MultiPolygon([g for g in res])
+            if res[0].area > house_area:
+                width -= dw
+            else:
+                width += dw
         area0 = Area(res[0], self._category)
         area1 = Area(res[1], new_category)
         if inplace:
@@ -141,5 +153,5 @@ class Area():
 
 if __name__ == "__main__":
     zone = Area(Polygon([(0,0), (10,0), (15,15), (-5,10)]), Category.HOUSE) # units are meters
-    zone.split(0.4, 180, inplace=True)  # house in south, it takes 40 % of the area
-    tools.json(zone, 'outfiles/house.json')
+    zone.split(0.4, 280, inplace=True)  # house in south, it takes 40 % of the area
+    tools.json(zone, '/tmp/house.json')
