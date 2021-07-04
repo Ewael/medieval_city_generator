86811f289bebbdfaac1cb0ffc8e2d8ccd4a286b3
Turodoras <turodoras@gmail.com>
2021-07-04 11:06:09 -0400

    Add package info installation

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="mcg",
+    version="1.0",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    install_requires=[
+        'shapely==1.7.1',
+        'fiona==1.8.20',
+        'geopandas==0.9.0',
+        'logging==0.4.9.6',
+        'pytest==6.2.4'
+        ]
+)
